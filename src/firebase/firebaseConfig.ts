d11ee8afdb134f9871874d79ab163c242d9ad4af
julia <63320916+araujuu@users.noreply.github.com>
2025-07-07 15:38:05 -0300

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaX7c-xfEA44bU1LVc_nb_qFHVOnvBTuM",
-  authDomain: "mercapp-e903c.firebaseapp.com",
-  projectId: "mercapp-e903c",
-  storageBucket: "mercapp-e903c.firebasestorage.app",
-  messagingSenderId: "113953025696",
-  appId: "1:113953025696:web:3c4c12aa73a4155d0da3c5",
-  measurementId: "G-EL6C64GJSV"
+  apiKey: "#",
+  authDomain: "#",
+  projectId: "#",
+  storageBucket: "#",
+  messagingSenderId: "#",
+  appId: "#",
+  measurementId: "#"
 };
 
 const app = initializeApp(firebaseConfig);
